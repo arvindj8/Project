df904e076f9f4a932f1d5231aef519be4eab56fd
Alexandr <buklov.a.a@yandex.ru>
2022-10-12 13:54:27 +0300

    Make one asking

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 "use strict";
 
-let incr = 10;
-let decr = 10;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// ++incr;
-// --decr;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(++incr);
-console.log(--decr); 
+const nameOfFilms1 = prompt('Один из последних просмотренных фильмов?', ''),
+      feedbackOfFilms1 = prompt('на сколько вы оцените его?', ''),
+      nameOfFilms2 = prompt('Один из последних просмотренных фильмов?', ''),
+      feedbackOfFilms2 = prompt('на сколько вы оцените его?', '');
+
+
+personalMovieDB.movies[nameOfFilms1] = feedbackOfFilms1;
+personalMovieDB.movies[nameOfFilms2] = feedbackOfFilms2;
+
+console.log(personalMovieDB);
 
